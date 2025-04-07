53e28673f799a5bdd4d9f877276f64119ab493c7
Khoi Vu <vu.khoi6111@gmail.com>
2025-04-06 21:19:11 -0600

    TranslateToDecimal routine added, I think it's correct?

--- event-tracker.asm
+++ event-tracker.asm
@@ -1,28 +1,38 @@
 .ORIG x3000
 
 Main:
+  AND R6, R6, #0     ; Initialize R6 to 0
+  ADD R6, R6, x4000
+  
   JSR HoursToDecimal
   JSR MinutesToDecimal
+  
+  HALT
 
+; Translate ASCII values to decimal
 TranslateToDecimal:
+  AND R0, R0, #0    ; Clear R0
+  ADD R0, R1, #-16  ; Subtract 16 from ASCII value
+  ADD R0, R0, #-16  ; Subtract 16 more
+  ADD R0, R0, #-16  ; Subtract 16 more (total -48 to convert from ASCII to decimal)
+  RET
 
-
+; Store Hours as decimal for ValidateHours Routine
 HoursToDecimal:
-  LD R1, HOURS_INPUT ; Load ASCII hour value to R1
+  LD R1, HOURS_INPUT    ; Load ASCII hour value to R1
   JSR TranslateToDecimal ; Call TranslateToDecimal
-  STR R0, R6, #0 ; Store decimal value of Hour in memory
-  RET ; Return 
+  STR R0, R6, #0        ; Store decimal value of Hour in memory
+  RET                   ; Return 
 
+; Store Minutes as decimal for ValidateMinutes Routine
 MinutesToDecimal:
-  LD R1, MINUTES_INPUT ; Load ASCII minute value to R1
+  LD R1, MINUTES_INPUT   ; Load ASCII minute value to R1
   JSR TranslateToDecimal ; Call TranslateToDecimal
-  STR R0, R6, #1 ; Store decimal value of Minute in memory
-  RET ; Return 
-
+  STR R0, R6, #1         ; Store decimal value of Minute in memory
+  RET                    ; Return 
 
 ; Add Sample Data
-HOURS_INPUT: .FILL X0032 ; Hours ASCII value to 2
-MINUTES_INPUT: .FILL X0038 ; Minutes ASCII value to 8
-ASCII_ZERO: .FILL X0030 ; ASCII value to 0
+HOURS_INPUT: .FILL X0032    ; Hours ASCII value to 2
+MINUTES_INPUT: .FILL X0038  ; Minutes ASCII value to 8
 
 .END
