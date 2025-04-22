09548bb5ff6657e399def5366c9d4bee69ab0abd
Khoi Vu <103450054+ChefKhoiRD@users.noreply.github.com>
2025-04-22 17:28:52 -0600

    Update Main.asm

--- Main.asm
+++ Main.asm
@@ -1,17 +1,27 @@
 .ORIG x3000
 
+    ; User Input Event Details (EventDetail.asm)
+    LD R0, EVENT_DETAIL_ADDR ; Loads address of EventDetail
+    JSRR R0                  ; Jump to subroutine at address in R0
+
     ; User Input Time Routine (TimeInput.asm)
-    LD R0, TIME_INPUT_ADDR  ; Load address of TimeInputRoutine
-    JSRR R0                 ; Jump to subroutine at address in R0
+    LD R0, TIME_INPUT_ADDR   ; Load address of TimeInputRoutine
+    JSRR R0                  ; Jump to subroutine at address in R0
+    
+    ; Displays User Inputted Event Details (DisplayEvent.asm)
+    LD R0, DISPLAY_ADDR      ; Loads address of DisplayEvent
+    JSRR R0                  ; Jump to subroutine at address in R0
     
     ; Calculate User Inputted Time Routine (TimeCalculation.asm)
-    LD R0, TIME_CALC_ADDR   ; Load address of CalculateEventTime
-    JSRR R0                 ; Jump to subroutine at address in R0
+    LD R0, TIME_CALC_ADDR    ; Load address of CalculateEventTime
+    JSRR R0                  ; Jump to subroutine at address in R0
     
     HALT             
 
 ; Address of external routines
-TIME_INPUT_ADDR    .FILL x4000  ; TimeInput.asm
-TIME_CALC_ADDR     .FILL x5000  ; TimeCalculation.asm
+EVENT_DETAIL_ADDR  .FILL x4000  ; EventDetail.asm
+TIME_INPUT_ADDR    .FILL x5000  ; TimeInput.asm
+DISPLAY_ADDR       .FILL x6000  ; DisplayEvent.asm
+TIME_CALC_ADDR     .FILL x7000  ; TimeCalculation.asm
 
 .END
