5ae85886daa33b0dfcccc2b49b4a8d0825b69462
Khoi Vu <103450054+ChefKhoiRD@users.noreply.github.com>
2025-04-22 01:21:18 -0600

    Update Main.asm

--- Main.asm
+++ Main.asm
@@ -1,8 +1,5 @@
 .ORIG x3000
 
-    LEA R0, SUNSET_HDR
-    PUTS
-    
     ; User Input Time Routine (TimeInput.asm)
     LD R0, TIME_INPUT_ADDR  ; Load address of TimeInputRoutine
     JSRR R0                 ; Jump to subroutine at address in R0
@@ -16,7 +13,5 @@
 ; Address of external routines
 TIME_INPUT_ADDR    .FILL x4000  ; TimeInput.asm
 TIME_CALC_ADDR     .FILL x5000  ; TimeCalculation.asm
-
-SUNSET_HDR         .STRINGZ "\nSunset Time: "
 
 .END
